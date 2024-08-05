eae762eae5a0b1ecc68816f4e42e957e42d22444
makerdegit <122148963+makerdegit@users.noreply.github.com>
2024-08-05 22:30:38 +0800

    Add files via upload

---
+++ Configuration Files/hadoop/etc/hadoop/my_env.sh
@@ -0,0 +1,20 @@
+#JAVA_HOME
+export JAVA_HOME=/opt/module/jdk1.8.0_212
+export PATH=$PATH:$JAVA_HOME/bin
+
+#HADOOP_HOME
+export HADOOP_HOME=/opt/module/hadoop
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
+
+#KAFKA_HOME
+export KAFKA_HOME=/opt/module/kafka
+export PATH=$PATH:$KAFKA_HOME/bin
+
+#HIVE_HOME
+export HIVE_HOME=/opt/module/hive
+export PATH=$PATH:$HIVE_HOME/bin
+
+# SPARK_HOME
+export SPARK_HOME=/opt/module/spark
+export PATH=$PATH:$SPARK_HOME/bin
